db18386f9b7abf63803efca4efd989622eabd3f6
RayArash81 <121951679+RayArash08@users.noreply.github.com>
2025-04-20 15:51:29 +0700

    Character controller added

--- ProjectHorrorWakBro/Assets/PlayerMovement.cs
+++ ProjectHorrorWakBro/Assets/PlayerMovement.cs
@@ -7,6 +7,9 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 12f;
+    public float gravity = -9.8f;
+
+    Vector3 velocity;
 
     // Update is called once per frame
     void Update()
@@ -17,6 +20,10 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
 
     }
 }
