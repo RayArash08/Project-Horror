c5d3f3e9753755b61cd010cbc151ec459b6a42d3
RayArash81 <121951679+RayArash08@users.noreply.github.com>
2025-04-20 16:04:33 +0700

    addeGravity

--- ProjectHorrorWakBro/Assets/PlayerMovement.cs
+++ ProjectHorrorWakBro/Assets/PlayerMovement.cs
@@ -9,11 +9,23 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 12f;
     public float gravity = -9.8f;
 
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
     Vector3 velocity;
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance,groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
